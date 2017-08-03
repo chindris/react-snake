c53fa7d5d6101ebc7703adf90cee49f48b5c38b7
Dan Lemon <daniel.lemon@amazee.com>
2017-08-03 09:31:26 +0200

    Add simple basic Snake component to be used for game

---
+++ src/Snake.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Snake extends Component {
+  render() {
+    return (
+      <div className="Snake">
+        To Do: code Snake
+      </div>
+    );
+  }
+}
+
+export default Snake;
